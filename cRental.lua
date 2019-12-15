e786c5f04cca1da3661a6759ed202c05e74af2e1
Dennis K <49080774+DorteY@users.noreply.github.com>
2019-12-15 14:52:50 +0100

    first upload

---
+++ cRental.lua
@@ -0,0 +1,55 @@
+--((Developers: DorteY))
+--((File: cRental.lua))
+
+local x,y=guiGetScreenSize()
+local GUI={Window={},Button={},Label={},Gridlist={},GridItem={}}
+
+local rentalUIvehicles={
+	{"Faggio",150},
+	{"Tampa",300},
+}
+
+function openRentalUI()
+	if(not isPedInVehicle(localPlayer))then
+		if(not isElement(GUI.Window[1]))then
+			showCursor(true)
+			GUI.Window[1]=guiCreateWindow(x/2-400/2,y/2-260/2,400,260,"Rental by DorteY",false)
+			guiWindowSetSizable(GUI.Window[1],false)
+			guiWindowSetMovable(GUI.Window[1],false)
+			
+			GUI.Gridlist[1]=guiCreateGridList(5,25,160,295,false,GUI.Window[1])
+			GUI.GridItem[1]=guiGridListAddColumn(GUI.Gridlist[1],"Vehicle",0.55)
+			GUI.GridItem[2]=guiGridListAddColumn(GUI.Gridlist[1],"Price",0.2)
+			
+			GUI.Label[1]=guiCreateLabel(180,25,240,30,"Here you can rent a vehicle for a limited\ntime.",false,GUI.Window[1])
+			GUI.Button[1]=guiCreateButton(180,215,200,35,"Rent vehicle",false,GUI.Window[1])
+			
+			for _,v in pairs(rentalUIvehicles)do
+				local row=guiGridListAddRow(GUI.Gridlist[1])
+				guiGridListSetItemText(GUI.Gridlist[1],row,GUI.GridItem[1],v[1],false,false)
+				guiGridListSetItemText(GUI.Gridlist[1],row,GUI.GridItem[2],v[2],false,false)
+			end
+			
+			
+			addEventHandler("onClientGUIClick",GUI.Button[1],
+				function(btn,state)
+					if(btn=="left" and state=="up")then
+						local selectedVeh=guiGridListGetItemText(GUI.Gridlist[1],guiGridListGetSelectedItem(GUI.Gridlist[1]),1)
+						local selectedPrice=tonumber(guiGridListGetItemText(GUI.Gridlist[1],guiGridListGetSelectedItem(GUI.Gridlist[1]),2))
+						if(clicked~="")then
+							if(selectedPrice~=nil)then
+								triggerServerEvent("rent:vehicle",localPlayer,selectedVeh,selectedPrice)
+								if(isElement(GUI.Window[1]))then
+									showCursor(false)
+									destroyElement(GUI.Window[1])
+								end
+							end
+						end
+					end
+				end,
+			false)
+		end
+	end
+end
+addEvent("open:rentalUI",true)
+addEventHandler("open:rentalUI",root,openRentalUI)
---
+++ sRental.lua
@@ -0,0 +1,63 @@
+--((Developers: DorteY))
+--((File: sRental.lua))
+
+--((Marker))
+RentalMarker=createMarker(-700,964,12,"cylinder",1.4,200,200,0,200,root)
+
+--((Tables))
+local rentVehicleTable={}
+local rentVehicleTimerTable={}
+
+function rentVehicle(typ,price)
+	if(isElementWithinMarker(client,RentalMarker))then
+		if(getPlayerMoney(client)>=price)then
+			if(typ and typ~=nil)then
+				if(typ=="Faggio")then
+					local x,y,z=getElementPosition(client)
+					local rx,ry,rz=getElementRotation(client)
+					
+					if(isElement(rentVehicleTable[client]))then
+						destroyElement(rentVehicleTable[client])
+					end
+					if(isTimer(rentVehicleTimerTable[client]))then
+						killTimer(rentVehicleTimerTable[client])
+					end
+					rentVehicleTable[client]=createVehicle(462,x,y,z,rx,ry,rz)
+				elseif(typ=="Tampa")then
+					local x,y,z=getElementPosition(client)
+					local rx,ry,rz=getElementRotation(client)
+					
+					if(isElement(rentVehicleTable[client]))then
+						destroyElement(rentVehicleTable[client])
+					end
+					if(isTimer(rentVehicleTimerTable[client]))then
+						killTimer(rentVehicleTimerTable[client])
+					end
+					rentVehicleTable[client]=createVehicle(549,x,y,z,rx,ry,rz)
+				end
+				
+				warpPedIntoVehicle(client,rentVehicleTable[client])
+				
+				rentVehicleTimerTable[client]=setTimer(function(client)
+					destroyElement(rentVehicleTable[client])
+					if(isTimer(rentVehicleTimerTable[client]))then
+						killTimer(rentVehicleTimerTable[client])
+					end
+				end,20*1000,1,client)
+				
+				setPlayerMoney(client,getPlayerMoney(client)-price)
+			end
+		else
+			outputChatBox("You don't have enough money! ($"..price..")",client,200,0,0)
+		end
+	end
+end
+addEvent("rent:vehicle",true)
+addEventHandler("rent:vehicle",root,rentVehicle)
+
+function openRentalUIfunc(player)
+	if(isElement(player))then
+		triggerClientEvent(player,"open:rentalUI",player)
+	end
+end
+addEventHandler("onMarkerHit",RentalMarker,openRentalUIfunc)
